867c1f0863368635448dc67cc70811b514ec736c
ubrnt <ubrnt.dev@gmail.com>
2026-04-26 17:54:07 +0100

    initial commit (project structure + build.gradle + .gitignore)

---
+++ src/main/java/ru/practicum/blog/config/AppConfig.java
@@ -0,0 +1,9 @@
+package ru.practicum.blog.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.practicum.blog")
+public class AppConfig {
+}
---
+++ src/main/java/ru/practicum/blog/config/WebConfig.java
@@ -0,0 +1,10 @@
+package ru.practicum.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+}
