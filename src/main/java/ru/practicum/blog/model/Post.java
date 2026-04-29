803998e3907e506204fee8e6705e9b511baf9dff
ubrnt <ubrnt.dev@gmail.com>
2026-04-29 11:56:32 +0100

    model implementation

---
+++ src/main/java/ru/practicum/blog/model/Post.java
@@ -0,0 +1,71 @@
+package ru.practicum.blog.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Post {
+
+    private Long id;
+    private String title;
+    private String text;
+    private byte[] image;
+    private int likesCount;
+    private int commentsCount;
+    private List<String> tags = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public int getLikesCount() {
+        return likesCount;
+    }
+
+    public void setLikesCount(int likesCount) {
+        this.likesCount = likesCount;
+    }
+
+    public int getCommentsCount() {
+        return commentsCount;
+    }
+
+    public void setCommentsCount(int commentsCount) {
+        this.commentsCount = commentsCount;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+}
